6b3a289bb7e7a1d89da341aa004059a03bff3326
SaurabhSonde <saurabhsonde111@gmail.com>
2021-03-26 19:14:09 +0530

    removed test code

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
 const express = require("express");
-const SpotifyWebApi = require("spotify-web-api-node");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const SpotifyWebApi = require("spotify-web-api-node");
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
-  console.log("hii");
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI,
     clientId: process.env.CLIENT_ID,
@@ -49,9 +50,7 @@ app.post("/login", (req, res) => {
       });
     })
     .catch((err) => {
-      console.log(err);
       res.sendStatus(400);
     });
 });
-
 app.listen(3001);
